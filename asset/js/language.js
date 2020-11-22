8882ffbd32b02b6b5c5981b5863d7d452d837317
ZazBah <zazbah88@gmail.com>
2020-11-22 12:18:41 +0600

    span add and js edited

--- asset/js/language.js
+++ asset/js/language.js
@@ -6,18 +6,42 @@ $(document).ready(function(){
 
   if(localStorage.getItem("prokria-language") === 'bangla'){
     $( "#bangla-button" ).prop("checked", true );
-    $.getJSON('/lang/bn.json',function(data) {
+    $.getJSON('./lang/bn.json',function(data) {
       //Transalation Will be bellow here
       $('.algorithmexplained').text(data.algorithmexplained);
       $('.sort').text(data.sort);
+      $('.bubble').text(data.bubble);
+      $('.selection').text(data.selection);
+      $('.insertion').text(data.insertion);
+      $('.heapsort').text(data.heapsort);
+      $('.merge').text(data.merge);
+      $('.quick').text(data.quick);
+      $('.clustering').text(data.clustering);
+      $('.k-means').text(data.k-means);
+      $('.listsearch').text(data.listsearch);
+      $('.linear').text(data.linear);
+      $('.binary').text(data.binary);
+      $('.simulate').text(data.simulate);
       //Transalation Will be above here
     });
   }else{
     $( "#english-button" ).prop("checked", true );
-    $.getJSON('/lang/en.json',function(data) {
+    $.getJSON('./lang/en.json',function(data) {
       //Transalation Will be bellow here
       $('.algorithmexplained').text(data.algorithmexplained);
       $('.sort').text(data.sort);
+      $('.bubble').text(data.bubble);
+      $('.selection').text(data.selection);
+      $('.insertion').text(data.insertion);
+      $('.heapsort').text(data.heapsort);
+      $('.merge').text(data.merge);
+      $('.quick').text(data.quick);
+      $('.clustering').text(data.clustering);
+      $('.k-means').text(data.k-means);
+      $('.listsearch').text(data.listsearch);
+      $('.linear').text(data.linear);
+      $('.binary').text(data.binary);
+      $('.simulate').text(data.simulate);
       //Transalation Will be above here
     });
   }
